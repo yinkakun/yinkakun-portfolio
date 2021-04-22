6fca48311b08980b0a9a34de3340f1255bef3a32
Yinka Adedire <yinkakun@gmail.com>
2021-04-22 10:48:42 +0100

    stop auto-scroll to the top on route change

---
+++ gatsby-browser.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  if (location.action === 'PUSH') {
+    window.setTimeout(() => window.scrollTo(0, 0));
+  } else {
+    const savedPosition = getSavedScrollPosition(location) || [0, 0];
+    window.setTimeout(
+      () => window.scrollTo(...savedPosition),
+      TRANSITION_DELAY
+    );
+  }
+  return false;
+};
